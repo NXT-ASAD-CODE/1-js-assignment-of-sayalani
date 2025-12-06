1d46052a0ef45e75d0ca64ba274ec6e2e501f6ab
NXT-ASAD-CODE <asadmulti786@gmail.com>
2025-12-06 12:37:05 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,162 @@
+// chapter 1 => Alerts
+// point Number:1
+// alert("hello world");
+// point Number:2
+// alert("asad");
+// point Number:3
+// alert(14);
+// point Number:4
+// var myname = "asad";
+// alert(myname);
+// point Number:5
+// var myname = " asad";
+// var greet = "Hello" + myname;
+// alert(greet)
+// chapter 1 end 
+
+
+// chapter 2 => Variables for Strings
+// point number : 1
+// var userName = "asad";
+// point number : 2
+// var greet = "Welcome";
+// alert(greet)
+// point number : 3
+// var firstname = "asad ";
+// var lastname = "iftikhar";
+// var fullname = firstname + lastname
+// alert(fullname);
+// point number : 4
+// var favoritequote = "Every 'Professional' is once a begineer! "
+// point number : 5
+// var city = "karachi";
+// alert("Welcome to city " + city  )
+// chapter 2 end
+
+
+// chapter 3 => Variables for Numbers
+// point number : 1
+// var age = 16;
+// point number : 2
+// var firstnumber = 10; 
+// var secoundnumber = 20; 
+// // var AdditionOfTwoNumber = firstnumber + secoundnumber;
+// // point number : 3
+// var firstnumber = 10; 
+// var secoundnumber = 20; 
+// var SubtractionOfTwoNumber = firstnumber - secoundnumber;
+// point number : 4 
+// var firstnumber = 10; 
+// var secoundnumber = 20; 
+// var MultiplicationOfTwoNumber = firstnumber * secoundnumber;
+// point number : 5
+// alert("you are 20 years old")
+// chapter 3 end
+
+
+// chapter 4 => Math Expressions
+// point number : 1
+// var x = 10;
+// var y = 5;
+// var addition = x + y;
+// var result = addition;
+// alert(result)
+// point number : 2
+// var x = 10;
+// var y = 5;
+// var subtraction = y - x;
+// var result = subtraction ;
+// alert(result)
+// point number : 3
+// var x = 10;
+// var y = 5;
+// var multiply = x * y;
+// var result = multiply;
+// alert(result)
+// point number : 4
+// var x = 10;
+// var y = 5;
+// var divide = x / y;
+// var result = divide;
+// alert(result)
+// point number : 5
+// var x = 10;
+// var y = 5;
+// var reminder = x % y;
+// var result = reminder;
+// alert(result)
+// chapter 4 end
+
+
+// chapter 5 => Prompt
+// point number : 1
+// var yourname = prompt("what is your name");
+// alert("asslamu walikum " + yourname)
+// point number : 2
+// var yourage = prompt("what is your age");
+// alert("your age is " + yourage)
+// point number : 3
+// var yournumber = prompt("Enter a Number For Double");
+// alert( 2 * yournumber)
+// point number : 4
+// var yourcolor = prompt("Enter a Color Name");
+// alert( "Wow! I like " + yourcolor + " too!" )
+// point number : 5
+// var yourcity = prompt("Enter a City Name");
+// alert( "You Are From " + yourcity)
+// chapter 5 end
+
+
+// chapter 6 => If Statement and Comparison Operators
+// point number : 1
+// var age = prompt("what is your age")
+// if(age < 30){
+//     alert("you are still young man")
+// }
+// else{
+//     alert("Do Not Tell Your Age To Any One")
+// }
+// point number : 2
+// var number = prompt("Enter a Number")
+// if(number % 2==0){
+//     alert("the number is even")
+// }
+// else{
+//     alert("the number is odd")
+// }
+// point number : 3
+// var firstnumber = prompt("Enter 1st Number")
+// var secoundnumber = prompt("Enter 2nd Number")
+// if(firstnumber>secoundnumber){
+//     alert(firstnumber + " is greater")
+// }
+// else{
+//     alert(secoundnumber + " is greater")
+// }
+// point number : 4
+// var password = prompt("Enter a Password")
+// if(password=="asad2428"){
+//     alert("you grant access")
+// }
+// else{
+//     alert("incorrect password")
+// }
+// point number : 5
+// var firstnumber = prompt("Enter 1st Number")
+// var secoundnumber = prompt("Enter 2nd Number")
+// if(firstnumber==secoundnumber){
+//     alert("The number is equel")
+// }
+// if(firstnumber===secoundnumber){
+//     alert("The number is equel with data type")
+// }
+// if(firstnumber!=secoundnumber){
+//     alert("The number is not equel")
+// }
+// if(firstnumber!==secoundnumber){
+//     alert("The number is not equel with data type")
+// }
+// chapter 6 end
+
+
+
